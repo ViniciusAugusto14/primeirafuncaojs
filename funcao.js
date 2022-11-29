a5d2b3216f6eeb6678eb7a63232d178c1094e4f8
Vinicius Silva Augusto <100208158+ViniciusAugusto14@users.noreply.github.com>
2022-11-29 08:54:34 -0300

    Add files via upload

--- funcao.js
+++ funcao.js
@@ -1,3 +1,35 @@
-function soma(a,b){
-    return a+b
-}
+    function soma(a,b){
+        return a+b
+    }
+
+    function subtr(a,b){
+        return a-b
+    }
+
+    function div(a,b){
+        return a/b
+    }
+
+    function multi(a,b){
+        return a*b
+    }
+
+    function decisao (resp,pri,seg){
+        if (resp==1){
+            resultado=soma(pri,seg)
+            document.write("<p>A soma é:"+ resultado)
+        }
+        if (resp==2){
+            resultado=subtr(pri,seg)
+            document.write("<p>A subtração é:"+ resultado)
+        }
+        if (resp==3){
+            resultado=div(pri,seg)
+            document.write("<p>A divisão é:"+ resultado)
+        }
+        if (resp==4){
+            resultado=multi(pri,seg)
+            document.write("<p>A multiplicação é:"+ resultado)
+        }
+        
+    }
