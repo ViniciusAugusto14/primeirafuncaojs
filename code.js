a455963324b25523948a40e306a7f34d5426ef68
Vinicius Silva Augusto <100208158+ViniciusAugusto14@users.noreply.github.com>
2023-02-07 11:07:15 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,17 @@
+let num1 = 4
+let num2 = 3
+function soma(n1,n2){
+   return n1 + 2
+}
+function sub(n1,n2){
+    return n1 - 2
+}
+function mult(n1,n2){
+    return n1*2
+}
+function div(n1,n2){
+    return n/2
+}
+
+
+console.log(soma(4))
